41db397f7be490d761ad14e62b385d03d06adf60
Vasconcellos <ricco.vasconcellos@sap.com>
2022-04-17 15:14:29 -0300

    Implement Add button to Note List

--- src/screens/note-list.js
+++ src/screens/note-list.js
@@ -1,6 +1,6 @@
 
 import React, {useState} from 'react';
-import {SafeAreaView, FlatList, Text} from 'react-native';
+import {SafeAreaView, FlatList, Text, View, Button} from 'react-native';
 
 import {COLORS} from '../styles/colors';
 import {styles} from '../styles/stylesheets';
@@ -20,9 +20,16 @@ export function NoteList() {
         contentContainerStyle={{alignItems:'flex-start', margin:20, paddingBottom:100}}
         data={notes.sort((a,b) => a.title > b.title)}
         //for debug: {'title:' + item.title + ' body:' + item.bodyText + ' color:' + item.color}
-        renderItem={({item}) => {return <Text style={styles.noteText}>
+        renderItem={({item}) => {return <View>
+        <Text style={styles.noteText}>
           {item.title + ' - ' + item.bodyText.substring(0,6)}
-          </Text>}}/>
+          </Text>
+          <Button 
+          onPress={() => {}} 
+          title='Add' 
+          color='#876759'/>
+          </View>}
+        }/>
     </SafeAreaView>
   );
 }
